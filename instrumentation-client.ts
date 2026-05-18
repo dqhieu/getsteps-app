eae1c2fcfabb654a22d0147c379b99d3c9f10c29
dqhieu <dqhieu13@gmail.com>
2026-05-18 22:44:18 +0700

    Disable session recording in PostHog initialization for improved user privacy.

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,6 +2,7 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
+  disable_session_recording: true,  
   ui_host: "https://us.posthog.com",
   capture_exceptions: true,
   person_profiles: "identified_only",
